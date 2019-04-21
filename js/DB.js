83df2d23810cf6c8f7f572a1f583fbd7442f3b45
wishower <49753852+wishower@users.noreply.github.com>
2019-04-21 18:38:43 +0800

    Update DB.js

--- js/DB.js
+++ js/DB.js
@@ -142,3 +142,29 @@ function UpdateDB(score,step,time,name){
       }, null);
    });
 }
+
+function open_soft_keyboard(options){
+
+   if(plus.os.name == 'iOS'){
+       setTimeout(function(){
+           var wv_current = plus.webview.currentWebview().nativeInstanceObject();
+           wv_current.plusCallMethod({"setKeyboardDisplayRequiresUserAction":false});
+           document.querySelector(options['input']).focus();
+       }, 330);
+   }else{
+       // 因为安卓autofocus只有4.0版本以上才支持，所以这里使用native.js来强制弹出
+       setTimeout(function(){
+           // 在执行的时候需要让当前webview获取焦点
+           var wv_current = plus.android.currentWebview();
+           plus.android.importClass(wv_current);
+           wv_current.requestFocus();
+
+           var Context = plus.android.importClass("android.content.Context");
+           var InputMethodManager = plus.android.importClass("android.view.inputmethod.InputMethodManager");
+           var main = plus.android.runtimeMainActivity();
+           var imm = main.getSystemService(Context.INPUT_METHOD_SERVICE);
+           imm.toggleSoftInput(0,InputMethodManager.SHOW_FORCED);
+           document.querySelector(options['input']).focus();
+       }, 330);
+   }
+}
